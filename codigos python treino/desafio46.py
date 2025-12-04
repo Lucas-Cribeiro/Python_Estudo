75e440fe4f16104aa32039dc2acf05328da1c063
Lucas Costa Ribeiro Fiscina <lucas.cribeiro@hotmail.com>
2025-12-04 17:12:30 -0300

    Atualização Repósitorio

---
+++ codigos python treino/desafio46.py
@@ -0,0 +1,7 @@
+import time
+print('Iniciando contagem regressiva...')
+time.sleep(0.6)
+for i in range (10, -1, -1):
+    print(f'{i}...')
+    time.sleep(1)
+print('LANÇANDO FOGOS!! BUUUUUUUUUUUUUUUUUUM\nKABUMMMMMMMMM')
